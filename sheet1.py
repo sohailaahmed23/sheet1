e8c367e34a9e1e5dc0fb1e9307f5df38f851df08
sohaila ahmed <sohailaahmed23@gamil.com>
2024-11-15 09:55:39 +0200

    added main file

---
+++ sheet1.py
@@ -0,0 +1,108 @@
+# writ a prodram to accept percentage from the user and display the grade according to the following criteria:
+# marks >90 , >80 and <=90 , >=60 and <=80 ,delow 60
+# geade A B C D 
+
+
+# marks = int(input('Enter your Grade:'))
+
+# if marks >90 :
+#     print('A')
+# elif marks >80 and marks <=90 :
+#     print('B')
+# elif marks >=60 and marks <=80 :
+#     print('c')
+# elif marks >60 :
+#     print('D')
+# else:
+#     print('error')
+
+
+#Q2
+# COST = float(input('Enter your tax'))
+# if COST >100000 :
+#     print('tax',0.15)
+# elif COST >50000 and COST <=100000 :
+#     print('tax',0.10)
+# elif COST <=50000 :
+#     print('tax',0.05)
+# else:
+#     print('tax',0)
+
+
+
+# Q5
+# num1 = int(input('Enter your first num'))
+# num2 = int(input('Enter your second num'))
+# if num1 > num2 :
+#     print('largest number')
+# else:
+#     print('small number')
+
+
+
+
+# Q6
+# number = int(input('Enter your number'))
+# if number >0:
+#     print('positive')
+# else:
+#     print('nagative')
+
+
+
+
+
+
+
+
+
+# Q9
+# num1 = int(input('Enter your num1'))
+# num2 = int(input('Enter your num2'))
+# num3 = int(input('Enter your num3'))
+# if num1 > num2 and num3 :
+#     print('largest numbrr')
+# else:
+#     print('small')
+
+
+                          #part2
+#Q1
+
+# Totaldays = int(input('Enter your num of working Days'))
+# absentdays = int(input('Enter your Days for absent'))
+# attendeddays = Totaldays - absentdays
+# attendancepercentage = (attendeddays/Totaldays)*100
+# print('attendancepercentage')
+# if attendancepercentage <75 :
+#     print('than student will not be able to sit in exam')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ shit2.py
@@ -0,0 +1,231 @@
+#Q1
+# x = True
+# print(x)
+
+#Q2
+# y = 10
+# print(y)
+
+#Q3
+# z = 0.12
+# print(z)
+
+#Q4
+# s = 'sohaila'
+# print(s)
+
+#Q5
+# x = 9
+# y = float(x)
+# print(y)
+
+#Q6
+# str = '123'
+# number =int(str)
+# print(number)
+
+#Q7
+# number = [1,2,3,4,5]
+# print(number)
+
+#Q8
+# number2 = (10,11,12,13,14,15)
+# print(number2)
+
+#Q9
+# number = [1,2,3,4,5]
+# tuple = tuple(number)
+# print(tuple)
+
+
+#Q10
+# members = {
+#     'name':'sohaila'
+#     ,'age': 23
+#     ,'active':True
+# }
+# print(members)
+
+#Q11
+# x = 10
+# print(x)
+
+#Q12
+# x = 5
+# x = x+15
+# print(x)
+
+#Q13
+# x = 25
+# x = x / 5
+# print(x)
+
+#Q14
+# x = 10
+# x =x*10
+# print(x)
+
+# Q15
+# x = 35
+# x =x%3
+# print(x)
+
+#Q16
+# x = 11 %4
+# print(x)
+
+#Q15
+# x = 11/3
+# print(x)
+
+#Q16
+# x = 11//3
+# print(x)
+
+#Q17
+# x = 10
+# y = 15
+# if x > y :
+#     print('x > y')
+# else:
+#     print('x <y')
+
+#Q18
+list = [2,4,6,8,10]
+if 4 in list :
+    print(4)
+else:
+    print('not found')
+if 4 in list and 6 in list :
+    print(4,6)
+else:
+    print('not found')
+if 3 in list or 6 in list :
+    print(3 or 6)
+if 2  in list  and 4 in list and 5 in list :
+    print(2,4,5)
+else:
+    print('notfound')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
